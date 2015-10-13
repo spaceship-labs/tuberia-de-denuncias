c217bd15486a8a8bbbaae097368ef4ea0b7d4ab0
Luis <luis19prz@gmail.com>
2015-10-13 15:18:07 -0500

    test fix

--- test/spec/controllers/conoce.js
+++ test/spec/controllers/conoce.js
@@ -17,7 +17,4 @@ describe('Controller: ConoceCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(ConoceCtrl.awesomeThings.length).toBe(3);
-  });
 });
