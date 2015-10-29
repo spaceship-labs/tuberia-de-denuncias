fc4894e118a22fc244b1e0f7ac9d3368bb3c76be
Luis <luis19prz@gmail.com>
2015-10-29 12:47:04 -0500

    test tipo-denuncia

--- test/spec/services/denunciaservice.js
+++ test/spec/services/denunciaservice.js
@@ -2,17 +2,48 @@
 
 describe('Service: denunciaService', function () {
 
+  //fixtures
+  before(function() {
+    fixture.setBase('test/fixtures');
+  });
+
   // load the service's module
   beforeEach(module('tuberiaPrototypeApp'));
 
   // instantiate service
-  var denunciaService;
-  beforeEach(inject(function (_denunciaService_) {
+  var denunciaService, $httpBackend;
+
+  beforeEach(inject(function (_$httpBackend_, _denunciaService_) {
     denunciaService = _denunciaService_;
+    $httpBackend = _$httpBackend_;
   }));
 
   /*it('should do something', function () {
     expect(!!denunciaService).toBe(true);
+  });*/
+
+  /*describe('denuncia', function() {
+
+    it('should create denuncia', function() {
+      this.denunciaCreated = fixture.load('denuncia-create.json');
+
+      var data = {
+        email:"test@test.com",
+        nombre:"test",
+        apellido:"test"
+      };
+
+      var params = angular.toJson({data:data});
+
+      $httpBackend.expectPOST('http://mte.spaceshiplabs.com/api/create_denuncia',params)
+      //$httpBackend.expectGET('http://mte.spaceshiplabs.com/api/create_denuncia?data={"email":"test@test.com","nombre":"test","apellido":"test"}')
+        .respond(this.denunciaCreated);
+
+      denunciaService.createDenuncia(data);
+      $httpBackend.flush();
+
+    });
+
   });*/
 
 });
--- test/spec/services/tipos-denuncia.js
+++ test/spec/services/tipos-denuncia.js
@@ -32,26 +32,39 @@ describe('Service: tiposDenuncia', function() {
       state.should.exist.and.equal(false);
     });
 
-    /*
-    it('should load tipos-denuncia', function(done) {
+
+    it('should load tipos-denuncia', function() {
       this.tiposJSON = fixture.load('tipos-denuncia.json');
       $httpBackend.expectGET('https://cdn.contentful.com:443/spaces/msnkbvgxrvah/entries?access_token=c3e13974fb72003886ab1f9151a883721f523e16f8143d95e92313ff1dd2c66e&content_type=1CQ8zB04qAuISUQwSEWUmA')
         .respond(this.tiposJSON);
+
+      tiposDenuncia.getList();
       $httpBackend.flush();
-      done();
+
     });
+
+    it('should load tipo-denuncia infraestructura', function() {
+      var typeId = '3JFuLY15te0mam6KeUu6kC';
+
+      this.tipoJSON = fixture.load('tipo-denuncia-infraestructura.json');
+      $httpBackend.expectGET('https://cdn.contentful.com:443/spaces/msnkbvgxrvah/entries?access_token=c3e13974fb72003886ab1f9151a883721f523e16f8143d95e92313ff1dd2c66e&sys.id='+typeId)
+        .respond(this.tipoJSON);
+
+      tiposDenuncia.getDenunciaType(typeId);
+      $httpBackend.flush();
+
+    });
+
 
     it('should return the current state', function() {
       var state = tiposDenuncia.getCurrentState();
       state.should.exist.and.be.an('object');
     });
 
-    */
+
 
   });
 
-
-  /*
 
   describe('changeState', function() {
     it('should advance to the next step and add one to the history', function() {
@@ -76,6 +89,6 @@ describe('Service: tiposDenuncia', function() {
     });
   });
 
-  */
+
 
 });
