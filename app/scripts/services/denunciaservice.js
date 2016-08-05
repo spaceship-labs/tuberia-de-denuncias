0f44730413c47d91c4a06fa804b416795858593c
urielaero <aero.uriel@gmail.com>
2016-08-05 01:32:57 -0500

    urls to prod

--- app/scripts/services/denunciaservice.js
+++ app/scripts/services/denunciaservice.js
@@ -12,8 +12,8 @@
   angular.module('tuberiaPrototypeApp')
     .service('denunciaService', function ($http) {
 
-      var baseUrl = 'http://comparatuescuela/api/';
-      //var baseUrl = 'https://mte.spaceshiplabs.com/api/';
+      //var baseUrl = 'http://comparatuescuela/api/';
+      var baseUrl = 'https://mte.spaceshiplabs.com/api/';
 
       this.getDenuncia = getDenuncia;
       this.createDenuncia = createDenuncia;
