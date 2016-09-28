9dd027fc2d3ef396438c868baf0d44940d7033a0
Luis <luis19prz@gmail.com>
2016-09-28 12:39:33 -0500

    fixes mobile

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,6 +75,7 @@ module.exports = function(grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
+        //hostname:'192.168.0.13',
         hostname: 'localhost',
         livereload: 35729
       },
